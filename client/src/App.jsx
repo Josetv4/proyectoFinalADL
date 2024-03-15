4d9d6f25f9caf8b58c055505935427b74b112de0
Josetv4 <joseflauta1993@gmail.com>
2024-03-15 01:33:34 -0300

    interface base update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import AboutUs from './pages/AboutUs';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import BeautyCatalog from './pages/BeautyCatalog';
+import MedicantionCatalog from './pages/MedicantionCatalog';
 
 
 function App() {
-
-
   return (
-    <>
-    </>
-  )
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/sobre-nosotros" element={<AboutUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/catalog1" element={<BeautyCatalog />} />
+        <Route path="/catalog2" element={<MedicantionCatalog />} />
+        <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,8 @@
+const loginPage = () => {
+    return (
+        <div >
+            <h1>Prueba pagina: Login </h1>
+        </div>
+    );
+    };
+    export default loginPage;
