cf6eb806a599b95aa704500742deb440d9720146
Yamil Castillo <yamilcs@gmail.com>
2024-03-15 22:16:06 -0300

    modificando Archivo userModel.js

--- server/src/api/v1/models/userModel.js
+++ server/src/api/v1/models/userModel.js
@@ -1,23 +1,57 @@
 import pool from "../../../../config/db/conectionDb.js";
 import bcrypt from "bcryptjs";
 
-const createUser = async ({ email, password, rol, lenguage }) => {
-  const hashedPassword = bcrypt.hashSync(password);
+const getUser = async () => {
 
   const SQLquery = {
-    text: "INSERT INTO usuarios (email, password, rol, lenguage) VALUES ($1, $2, $3, $4) RETURNING *",
-    values: [email, hashedPassword, rol, lenguage],
+    text: "SELECT user_id, username, email, phone, password, shipping_address, payment_method, role, status  from users"  
+   };
+  
+  const response = await pool.query(SQLquery);
+  return response.rows[0];
+};
+
+const getUserId = async ( id ) => {
+
+  const SQLquery = {
+    text: "SELECT user_id, username, email, phone, password, shipping_address, payment_method, role, status WHERE user_id = $1 from users",
+    values: [ id ],  
+   };
+  
+  const response = await pool.query(SQLquery);
+  return response.rows[0];
+};
+
+const createUser = async ({ username, email, phone, password, shipping_address, payment_method, role, status }) => {
+
+  const hashedPassword = bcrypt.hashSync(password);
+  const SQLquery = {
+    text: "INSERT INTO users (username, email, phone, password, shipping_address, payment_method, role, status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+    values: [username, email, phone, hashedPassword, shipping_address, payment_method, role, status],
   };
   const response = await pool.query(SQLquery);
   return response.rows[0];
 };
-const updateUser = async ({ email, password, rol, lenguage }) => {
-    const hashedPassword = bcrypt.hashSync(password);
+
+const updateUser = async ( id , { username, email, phone, password, shipping_address, payment_method, role, status }) => {
   
+    const hashedPassword = bcrypt.hashSync(password);
     const SQLquery = {
-      text: "INSERT INTO users () VALUES ($1, $2, $3, $4) RETURNING *",
-      values: [email, hashedPassword, rol, lenguage],
+      text: "UPDATE users SET username = $1, email = $2, phone = $3, password = $4, shipping_address = $5, payment_method = $6, role = $7, status = $8 WHERE user_id = $9 RETURNING *",
+      values: [ username, email, phone, hashedPassword, shipping_address, payment_method, role, status, id ],
     };
     const response = await pool.query(SQLquery);
     return response.rows[0];
   };
+
+  const deleteUser = async ( id ) => {
+  
+    const SQLquery = {
+      text: "DELETE FROM user_id = $1 RETURNING *",
+      values: [ id ],
+    };
+    const response = await pool.query(SQLquery);
+    return response.rows[0];
+  };
+
+  export { getUser, getUserId, createUser, deleteUser, updateUser };
