1fe3cdf181010ca866162853cd60931166592500
Yamil Castillo <yamilcs@gmail.com>
2024-03-17 19:33:43 -0300

    Se agrega middleware validateParametersUser

--- server/config/routes/userRoutes.js
+++ server/config/routes/userRoutes.js
@@ -1,13 +1,11 @@
 import express from "express";
 
- import { createNewUser, getUsers, getUsersId, updateUsers, deleteUsers } from "../../src/api/v1/controllers/usersController.js";
+import { createNewUser, getUsers, getUsersId, updateUsers, deleteUsers } from "../../src/api/v1/controllers/usersController.js";
 
 
-/*
+
 import { validateParametersUser } from "../../middlewares/validateParametersUser.js";
-import { validateParametersLogin } from "../../middlewares/validateParametersLogin.js";
-import { authToken } from "../../middlewares/authToken.js"
-*/
+
 import { getActivity } from "../../middlewares/reports.js"; 
 
 
@@ -16,7 +14,7 @@ const router = express.Router();
 
 router.get("/users", getActivity, getUsers )
 router.get("/users/:id", getActivity, getUsersId )
-router.post("/users/", getActivity, createNewUser )
+router.post("/users/", validateParametersUser,  getActivity, createNewUser )
 router.put("/users/:id", getActivity, updateUsers )
 router.delete("/users/:id", getActivity, deleteUsers )
 
