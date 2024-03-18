cf9a6a6def97cc4f2939407fe6be87907fab51a7
Josetv4 <joseflauta1993@gmail.com>
2024-03-18 11:11:04 -0300

    buttonCark

---
+++ client/src/utils/CartButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    Box,
+    Typography,
+    Button,
+    Tooltip,
+} from '@mui/material';
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+
+
+const UserLogged = () => {
+    return (
+        <Box>
+            <MdOutlineShoppingCart />
+        </Box>
+    );
+};
+
+export default UserLogged;
