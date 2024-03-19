794cc38e8a404fe531c7be9e2fec43f4c08a204b
ingrid-mo <morales.ingrid.i20@gmail.com>
2024-03-18 22:54:05 -0300

    cambios develop

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
+
 import Regiter from "../components/register/regiter";
-=======
-import Regiter from "../components/register/Regiter";
->>>>>>> 8c6bbbaf538a8a8c1f6ef07e4a0bf437c307f6c7
+
+
 
 const registerPage = () => {
     
