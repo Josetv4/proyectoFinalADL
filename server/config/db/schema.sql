6fecbd766a4ae2bb6e01b4ab445ac63d5d52b8eb
Yamil Castillo <yamilcs@gmail.com>
2024-03-20 21:35:41 -0300

    cambios en el productController para cargar imagen

--- server/config/db/schema.sql
+++ server/config/db/schema.sql
@@ -23,10 +23,10 @@ CREATE TABLE Products (
     name VARCHAR(255) NOT NULL,
     description TEXT,
     price DECIMAL(10, 2) NOT NULL,
-    quantity INT NOT NULL,
+    stock INT NOT NULL,
     category_id INT REFERENCES categories(category_id),
-    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    post_status VARCHAR(50) NOT NULL,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50) NOT NULL,
     user_id INT REFERENCES Users(user_id),
     image_url VARCHAR(255)
 );
@@ -38,60 +38,28 @@ VALUES
 -- Crear la tabla de Categorías
 CREATE TABLE categories (
     category_id SERIAL PRIMARY KEY,
-    name VARCHAR(100)  NOT NULL
- 
+    name VARCHAR(100)  NOT NULL,
+     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 INSERT INTO categories(name) VALUES('Belleza'),
-('Anticonceptivos'),
-('Antidepresivos'),
-('Antipsicóticos'),
-('Analgésicos'),
-('Antipiréticos'),
-('Antidiarreicos'),
-('Antihipertensivos'),
-('Antiinflamatorios'),
-('Antialérgicos'),
-('Antivirales'),
-('Diabetes'),
-('Suplementos alimenticios'),
-('Oftalmológicos'),
-('Vitaminas y minerales'),
-('Laxantes'),
-('Tiroides');
-
-
--- Crear la tabla de Comentarios y valoraciones
-CREATE TABLE Reviews (
-    review_id SERIAL PRIMARY KEY,
-    product_id INT REFERENCES Products(product_id),
-    user_id INT REFERENCES Users(user_id),
-    rating INT NOT NULL,
-    comment TEXT,
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-INSERT INTO Reviews (product_id, user_id, rating, comment)
-VALUES 
-    (1, 1, 5, '¡Excelente producto, lo recomiendo totalmente!'),
-    (2, 2, 4, 'Buen producto, pero podría mejorar en algunos aspectos.');
-
--- Crear la tabla de Mensajes
-CREATE TABLE Messages (
-    message_id SERIAL PRIMARY KEY,
-    sender_id INT REFERENCES Users(user_id),
-    recipient_id INT REFERENCES Users(user_id),
-    content TEXT,
-    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-INSERT INTO Messages (sender_id, recipient_id, content)
-VALUES 
-    (1, 2, 'Hola, ¿cómo estás?'),
-    (2, 1, '¡Hola! Estoy bien, gracias por preguntar.');
-
-
-
+                            ('Anticonceptivos'),
+                            ('Antidepresivos'),
+                            ('Antipsicóticos'),
+                            ('Analgésicos'),
+                            ('Antipiréticos'),
+                            ('Antidiarreicos'),
+                            ('Antihipertensivos'),
+                            ('Antiinflamatorios'),
+                            ('Antialérgicos'),
+                            ('Antivirales'),
+                            ('Diabetes'),
+                            ('Suplementos alimenticios'),
+                            ('Oftalmológicos'),
+                            ('Vitaminas y minerales'),
+                            ('Laxantes'),
+                            ('Tiroides');
 -- Crear la tabla de Cabecera de Carrito de compras
-CREATE TABLE ShoppingCartHeader (
+CREATE TABLE cart (
     cart_id SERIAL PRIMARY KEY,
     user_id INT REFERENCES Users(user_id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -102,73 +70,40 @@ INSERT INTO ShoppingCartHeader (user_id, status)
  (1, 'Abierto');
 
 -- Crear la tabla de Detalles de Carrito de compras
-CREATE TABLE ShoppingCartDetail (
+CREATE TABLE cart_items (
     detail_id SERIAL PRIMARY KEY,
-    cart_id INT REFERENCES ShoppingCartHeader(cart_id),
-    product_id INT REFERENCES Products(product_id),
+    cart_id INT REFERENCES cart(cart_id),
+    product_id INT REFERENCES products(product_id),
     quantity INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
+    price DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
-INSERT INTO ShoppingCartDetail (cart_id, product_id, quantity, price)
+INSERT INTO Cart_Items (cart_id, product_id, quantity, price)
 VALUES (1, 1, 2, 9.99),
 (2, 3, 1, 24.99);
 
---Alternativas
--- Crear la tabla de Órdenes
-CREATE TABLE Orders (
-    order_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES Users(user_id),
-    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(50) NOT NULL,
-    shipping_address VARCHAR(255)
-);
-
--- Crear la tabla de Detalles de la orden
-CREATE TABLE OrderDetails (
-    order_detail_id SERIAL PRIMARY KEY,
-    order_id INT REFERENCES Orders(order_id),
-    product_id INT REFERENCES Products(product_id),
-    quantity INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
-);
--- Crear la tabla de Transacciones
-CREATE TABLE Transactions (
-    transaction_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES Users(user_id),
-    amount DECIMAL(10, 2) NOT NULL,
-    transaction_type VARCHAR(50) NOT NULL,
-    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Opcional, hay que definir 
--- si los productos se postean o ya estan ingresados
-
-CREATE TABLE ProductPosts (
-    post_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES Users(user_id),
-    product_id INT REFERENCES Products(product_id),
-    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(50) NOT NULL
-);
-
--- Se podrian crear 2 tablas y asi separar la info del posteo de productos
--- Crear la tabla de Productos
-CREATE TABLE Products (
-    product_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES Users(user_id),
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    price DECIMAL(10, 2) NOT NULL,
-    quantity INT NOT NULL,
-    category VARCHAR(100),
-    image_url VARCHAR(255)
-);
-
--- Crear la tabla de Publicaciones
-CREATE TABLE ProductPosts (
-    post_id SERIAL PRIMARY KEY,
+-- Crear la tabla de Comentarios y valoraciones
+CREATE TABLE Reviews (
+    review_id SERIAL PRIMARY KEY,
     product_id INT REFERENCES Products(product_id),
     user_id INT REFERENCES Users(user_id),
-    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    post_status VARCHAR(50) NOT NULL
+    rating INT NOT NULL,
+    comment TEXT,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+INSERT INTO Reviews (product_id, user_id, rating, comment)
+VALUES 
+    (1, 1, 5, '¡Excelente producto, lo recomiendo totalmente!'),
+    (2, 2, 4, 'Buen producto, pero podría mejorar en algunos aspectos.');
+
+
+CREATE TABLE Favorites (
+    favorites_id SERIAL PRIMARY KEY,
+    product_id INT REFERENCES Products(product_id),
+    user_id INT REFERENCES Users(user_id),
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO Favorites (favorites_id, product_id, user_id)
+VALUES 
+    (1, 1, 1),
+    (2, 2, 1);
