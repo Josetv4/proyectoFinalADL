b686db2d3c2509ec7fd8afd2aa7779d5782ddc87
ingrid-mo <morales.ingrid.i20@gmail.com>
2024-03-25 11:37:53 -0300

    termine page de login y registro

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -12,7 +12,7 @@ const loginPage = () => {
         display="flex"
                 >
           <Grid container spacing={2} columns={16}>
-            <Grid item xs={8}>
+            <Grid item xs={8}  >
               <div className="login_img">
                 <img
                   src="https://firebasestorage.googleapis.com/v0/b/farmacias-syg.appspot.com/o/imagenes%2FGroup%201002.png?alt=media&token=6f465b19-a0c9-4324-8e6b-8d765a9ebb6e"
