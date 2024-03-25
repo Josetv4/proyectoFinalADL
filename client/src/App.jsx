3c69a7f620a818e5e2d09234aa069e299b774128
Josetv4 <joseflauta1993@gmail.com>
2024-03-25 12:48:38 -0300

    APP.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,8 +33,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/sobre-nosotros" element={<AboutUs />} />
-              <Route path="/login" element={<Login
-               />} />
+              <Route path="/login" element={<Login/>} />
               <Route path="/register" element={<Register />} />
               <Route path="/catalog1" element={<BeautyCatalog />} />
               <Route path="/catalog2" element={<MedicantionCatalog />} />
