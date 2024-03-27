8cc1a42c611553c9bcb01dfe8a198c9d997146d2
Yamil Castillo <yamilcs@gmail.com>
2024-03-27 00:19:53 -0300

    modificando LastShopping formato card

--- client/src/components/ShoppingCard/ShoppingCard.jsx
+++ client/src/components/ShoppingCard/ShoppingCard.jsx
@@ -1,11 +1,11 @@
-import {
-  Card,
-  CardContent,
-  CardActions,
-  Button,
-  Typography,
-  Container
-} from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 import "./style.css";
 
@@ -14,21 +14,24 @@ const ShoppingCard = () => {
     {
       id: 1,
       fecha: "2024-03-25",
-      imagen: "https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg",
+      imagen:
+        "https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg",
       descripcion: "Descripción del producto 1",
       precio: "$10.99",
     },
     {
       id: 2,
       fecha: "2024-03-24",
-      imagen: "https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg",
+      imagen:
+        "https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg",
       descripcion: "Descripción del producto 2",
       precio: "$15.49",
     },
     {
       id: 3,
       fecha: "2024-03-23",
-      imagen: "https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg",
+      imagen:
+        "https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg",
       descripcion: "Descripción del producto 3",
       precio: "$8.25",
     },
@@ -36,32 +39,37 @@ const ShoppingCard = () => {
   return (
     <main>
       <Container>
+        <h1>Tus ultimas compras</h1>
+        {lastShopping.map((item) => (
+          <Card key={item.id} className="shopping-card">
+            <CardContent className="shopping-card-content">
+              <Typography variant="p" className="shopping-card-color">
+                Fecha de Compra
+              </Typography>
+              <Typography variant="p">{item.fecha}</Typography>
+            </CardContent>
+            <CardContent className="shopping-box-content">
+              <Box>
+                <img
+                  className="shopping-card-image"
+                  src={item.imagen}
+                  alt={item.descripcion}
+                />
+              </Box>
+              <Box className="shopping-card-content">
+                <Typography variant="p">{item.descripcion}</Typography>
+                <Typography variant="p" className="shopping-card-color">{item.precio}</Typography>
+                <Typography variant="p">Vendido por <a href="#">petcos Spa</a></Typography>
 
-      
-      { lastShopping.map((item) => (
-        <Card key={item.id} className="shopping-card">
-          <CardContent>
-           <Typography variant="p">{item.fecha}</Typography>
-            <Typography variant="body1">{item.fecha}</Typography>
-          </CardContent>
-
-          <CardContent>
-            <img
-              className="product-image"
-              src={item.imagen}
-              alt={item.descripcion}
-            />
-            <Typography variant="p">{item.descripcion}</Typography>
-            <Typography variant="p">{item.precio}</Typography>
-          </CardContent>
-
-          <CardActions>
-            <Button variant="contained" color="primary">
-              Comentarios
-            </Button>
-          </CardActions>
-        </Card>
-      ))}
+              </Box>
+            </CardContent>
+            <CardActions sx={{ marginRight:2}}>
+              <Button variant="contained" color="primary">
+                Comenta tus productos
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
       </Container>
     </main>
   );
