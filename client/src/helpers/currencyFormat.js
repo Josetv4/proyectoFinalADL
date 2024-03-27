7d384f67cd163039c893d77e3ae5d6bff0565542
Neyeska Medina <neyeska.go@gmail.com>
2024-03-26 23:37:57 -0300

    Add hook handleCart, helper currency-format and folder in page ShoppingCart

---
+++ client/src/helpers/currencyFormat.js
@@ -0,0 +1,5 @@
+export const currencyFormat = new Intl.NumberFormat('es-CL', {
+    style: 'currency',
+    currency: 'CLP',
+    minimumFractionDigits: 0
+})
---
+++ client/src/pages/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,7 @@
+
+
+const ShoppingCart = () =>{
+
+}
+
+export default ShoppingCart;
