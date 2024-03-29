89f4e6ff2a18e9a158045e118d3069db3f492354
Yamil Castillo <yamilcs@gmail.com>
2024-03-29 01:37:39 -0300

    modificando server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname, path} from 'path';
 
 import { logger } from "logger-express";
 
