f1267c5de44e0c2243a09b09d98b298fb2b6d973
Maria Jose Cardenas <cotee.cr@gmail.com>
2024-04-02 23:58:29 -0300

    integración page Category con back , funcionalidad ordenamiento

--- client/src/components/Navbar/DrawerListCategory.jsx
+++ client/src/components/Navbar/DrawerListCategory.jsx
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import  { useEffect, useState } from "react";
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
@@ -6,19 +6,7 @@ import Collapse from '@mui/material/Collapse';
 import { ExpandLess, ExpandMore } from '@mui/icons-material';
 import { useNavigate } from "react-router-dom";
 import { buttonStyles } from './ButtonStyleHoverFocus';
-
-const categories = [
-    { nombre: "Belleza", id: 1 },
-    { nombre: "Anticonceptivos", id: 2 },
-    { nombre: "Antidepresivos", id: 3 },
-    { nombre: "Antipsicóticos", id: 40 },
-    { nombre: "Analgésicos", id: 32 },
-    { nombre: "Antipiréticos", id: 12 },
-    { nombre: "Antidiarreicos", id: 414 },
-    { nombre: "Antihipertensivos", id: 123 },
-    { nombre: "Oftamológico", id: 432 },
-    { nombre: "Diabetes", id: 7 },
-];
+import { getCategories } from "../../api/getApi";
 
 const buttonStyles1 = {
     color: 'var(--font-navbar-color3)',
@@ -37,17 +25,27 @@ const buttonStyles2 = {
 const ListCategory = ({ NavLinkComponent }) => {
     const [open, setOpen] = useState(false);
     const [selectedCategory, setSelectedCategory] = useState(null);
+    const [categories,setCategories] = useState([]);
     const navigate = useNavigate();
+
+
+    useEffect(()=>{
+      asyncGetCategories();
+    },[]);
 
     const handleToggle = (event) => {
         event.stopPropagation();
         setOpen(!open);
     };
 
-    const handleCategoryClick = (categoryId, categoryName) => {
-        setSelectedCategory(categoryId);
-        navigate(`/category/${categoryId}/${categoryName}`);
-    };
+    const asyncGetCategories = async()=>{
+        try {
+            const response = await getCategories();
+            setCategories(response.response.category)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <div>
@@ -61,16 +59,15 @@ const ListCategory = ({ NavLinkComponent }) => {
                 </ListItemButton>
                 <Collapse in={open} timeout="auto" unmountOnExit>
                     <List component="div" disablePadding>
-                        {categories.map((category) => (
+                        {categories && categories.map((category) => (
                             <ListItemButton
-                                key={category.id}
+                                key={category.category_id}
                                 sx={{...buttonStyles, ...buttonStyles1}}
-                                selected={selectedCategory === category.id}
-                                onClick={() => handleCategoryClick(category.id, category.nombre)}
+                                selected={selectedCategory === category.category_id}
                                 component={NavLinkComponent}
-                                to={`/category/${category.id}/${category.nombre}`}
+                                to={`/category/${category.category_id}/${category.name}`}
                             >
-                                <ListItemText primary={category.nombre} />
+                                <ListItemText primary={category.name} />
                             </ListItemButton>
                         ))}
                     </List>
