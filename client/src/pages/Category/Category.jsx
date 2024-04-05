c947a352223467a62ff9337493fa6d15ff08a9a3
Maria Jose Cardenas <cotee.cr@gmail.com>
2024-04-05 00:15:07 -0300

    mejoras page Categoria, fix servicio que trae productos por categoria

--- client/src/pages/Category/Category.jsx
+++ client/src/pages/Category/Category.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import { useEffect, useState } from "react";
 import { Box, FormControl, Grid, InputLabel, MenuItem, Select, Typography, formControlClasses } from "@mui/material";
@@ -9,120 +9,40 @@ import "./styles.css";
 import ButtonLittle from "../../components/Buttons/buttonLittle/buttonLittle";
 import { getProductsByCategory } from "../../api/getApi";
 
-let arrayProducts = [
-  {
-    name: 'Lozartan',
-    image: 'https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg',
-    description: 'Descripción del medicamento A.',
-    format: '30 Comprimidos Recubiertos',
-    price: 10.990,    
-    valoration : 3.5,
-    seller : "Petco SPA",
-    id : 1
-  },
-  {
-    name: 'Amoxicilina',
-    image: 'https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg',
-    description: 'Descripción del medicamento A.',
-    format: '30 Comprimidos Recubiertos',
-    price: 10.990,
-    valoration : 3.5,
-    seller : "Petco SPA",
-    id : 1
-  },
-  {
-    name: 'Lozartan',
-    image: 'https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg',
-    description: 'Descripción del medicamento A.',
-    format: '30 Comprimidos Recubiertos',
-    price: 10.990,
-    valoration : 3.5,
-    seller : "Petco SPA",
-    id : 1
-  },
-  {
-    name: 'Lozartan',
-    image: 'https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg',
-    description: 'Descripción del medicamento A.',
-    format: '30 Comprimidos Recubiertos',
-    price: 990,
-    valoration : 3.5,
-    seller : "Petco SPA",
-    id : 1
-  },
-  {
-    name: 'Lozartan',
-    image: 'https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg',
-    description: 'Descripción del medicamento A.',
-    format: '30 Comprimidos Recubiertos',
-    price: 10.990,
-    valoration : 3.5,
-    seller : "Petco SPA",
-    id : 1
-  },
-  {
-    name: 'Lozartan',
-    image: 'https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg',
-    description: 'Descripción del medicamento A.',
-    format: '30 Comprimidos Recubiertos',
-    price: 10.990,
-    valoration : 3.5,
-    seller : "Petco SPA",
-    id : 1
-  },
-  {
-    name: 'Lozartan',
-    image: 'https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg',
-    description: 'Descripción del medicamento A.',
-    format: '30 Comprimidos Recubiertos',
-    price: 10.990,
-    valoration : 3.5,
-    seller : "Petco SPA",
-    id : 1
-  },
-];
-
 const Category = () => {
   const { id, name } = useParams();
   const [products,setProducts] = useState([]);
-  const [orderPrice,setOrderPrice] = useState(0);
-  const [orderAz,setOrderAz] = useState(0);
+
+  const {state} = useLocation()
 
   useEffect(()=>{
     asyncGetProducts();
-  },[]);
+  },[state]);
 
   const asyncGetProducts = async() =>{
     try {
       const response = await getProductsByCategory(id);
       console.log(response)
-      //setProducts(response.response.response)
+      //setProducts(arrayProducts);
+      setProducts(response.response)
     } catch (error) {
       console.log(error);
     }
   }
 
-  const handleChangeOrderPrice = (event) =>{
-    console.log(event)
-    setOrderPrice(event.target.value);
-  }
-
-  const handleChangeOrderAz = (event) =>{
-    console.log(event)
-    setOrderAz(event.target.value);
-  }
-
-  const handleClickFilter = (event) =>{
+  const handleChangeOrder = (event) =>{
     //Ordenar
-    if(orderPrice === 1){
-      arrayProducts.sort((a,b) =>b.price - a.price);
-      console.log(arrayProducts);
-    }else if(orderPrice === 2){
-      arrayProducts.sort((a,b) =>a.price - b.price);
-      console.log(arrayProducts);
+    const order = event.target.value;
+    const newOrder = [...products];
+    if(order === 1){
+      newOrder.sort((a,b) =>a.price - b.price);
+      console.log(newOrder);
+    }else if(order === 2){
+      newOrder.sort((a,b) =>b.price - a.price);
+      console.log(newOrder);
     }
-    else if(orderAz === 1){
-      arrayProducts.sort((a,b) =>{
+    else if(order === 3){
+      newOrder.sort((a,b) =>{
         if (a.name > b.name) {
           return 1;
         }
@@ -131,10 +51,10 @@ const Category = () => {
         }
         return 0;
       });
-      console.log(arrayProducts);
+      console.log(newOrder);
     }
-    else if(orderAz === 2){
-      arrayProducts.sort((a,b) =>{
+    else if(order === 4){
+      newOrder.sort((a,b) =>{
         if (a.name < b.name) {
           return 1;
         }
@@ -143,65 +63,46 @@ const Category = () => {
         }
         return 0;
       });
-      console.log(arrayProducts);
+      console.log(newOrder);
     }
+    
+    setProducts(newOrder)
+    console.log("products: ",products)
   }
 
   return (
     <>
-      <Grid container spacing={2} sx={{justifyContent:"space-evenly"}}>
+      <Grid container spacing={1} sx={{display:"flex", flexDirection:"column", alignContent:"center", marginTop:"10px"}}>
         <Grid 
         item 
-        xs={2}
-        sx={{display:"flex", flexDirection:"column",alignItems:"center"}}>
-          <Typography className="order-by-box" sx={{marginTop:"100px", fontSize:"25px", textAlign:"center"}}>
-            Ordenar Por
+        xs={10}
+        sx={{display:"flex", justifyContent:"space-between"}}>
+          <Typography variant="h4" color="textSecondary" >
+            {name}
           </Typography>
-          <Box className="box-selects">
+          <Box>
             <FormControl>
-              <InputLabel id="first-select-label">Precio</InputLabel>
               <Select
                 className="order-select"
-                labelId="price-order-label"
-                id="price-order"
                 IconComponent={()=> (<IoIosArrowDown className="arrow-select"/>)}
-                onChange={handleChangeOrderPrice}                
+                onChange={handleChangeOrder}                
                 defaultValue={0}
               >
-                <MenuItem value={0}>Seleccione...</MenuItem>
+                <MenuItem value={0}>Ordenar por</MenuItem>
                 <MenuItem value={1}>Menor a mayor precio</MenuItem>
                 <MenuItem value={2}>Mayor a menor precio</MenuItem>
+                <MenuItem value={3}>A-Z</MenuItem>
+                <MenuItem value={4}>Z-A</MenuItem>
               </Select>
             </FormControl>
-            <FormControl >
-              <InputLabel id="second-select-label">Alfabéticamente</InputLabel>
-              <Select 
-                className="order-select"
-                labelId="alfabeticamente-order-label"
-                id="alfabeticamente-order"
-                IconComponent={()=> (<IoIosArrowDown className="arrow-select"/>)}
-                onChange={handleChangeOrderAz}
-                defaultValue={0}
-              >
-                <MenuItem value={0}>Seleccione...</MenuItem>
-                <MenuItem value={1}>A-Z</MenuItem>
-                <MenuItem value={2}>Z-A</MenuItem>
-              </Select>
-            </FormControl>
-            <ButtonLittle onClick={handleClickFilter}>
-              Filtrar
-            </ButtonLittle>
           </Box>
         </Grid>
         <Grid 
           item
-          xs={10}
-          sx={{marginTop:"3%"}}>
-          <Typography variant="h4" color="textSecondary" >
-            {name}
-          </Typography>
+          xs={12}
+        >
           <Box sx={{ display: "grid", gridTemplateColumns: "repeat(4,1fr)", marginTop: "10px", gap: "15px" }}>
-            {arrayProducts.map((product, i) =>
+            {products && products.map((product, i) =>
               <ProductCard key={i} product={product} />
             )}
           </Box>
