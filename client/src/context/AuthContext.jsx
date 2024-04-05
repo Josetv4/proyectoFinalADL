8da1c3bfe85433cd89ab4eaf8635a9590bb9c83c
Josetv4 <joseflauta1993@gmail.com>
2024-04-05 17:30:21 -0300

    se corrigen errores

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,5 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import usersData from "../components/json/userData.json";
+import { createContext, useContext, useState } from "react";
 import { userRegister, loginUser } from "../api/getApi.js";
 
 const AuthContext = createContext();
@@ -7,26 +6,10 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [userId, setUserId] = useState(null);
-    
-
-   /*  useEffect(() => {
-        const token = localStorage.getItem("token");
-        if (token) {
-            const userFromToken = Object.values(usersData).find(user => user.token === token);
-            if (userFromToken) {
-                setUser(userFromToken);
-            }
-        }
-    }, []); */
-
     const login = async (userData) => {
 
         const userToLogin = await loginUser(userData)
         console.log(userToLogin);
-
-        /* const userToLogin = Object.values(usersData).find(user =>
-            user.email === credentials.email && user.password === credentials.password
-        ); */
         if (userToLogin) {
             setUser(userToLogin);
             localStorage.setItem("token", userToLogin.response.token);
@@ -42,12 +25,12 @@ export const AuthProvider = ({ children }) => {
 
     const register = async (userData) => {
         try {
-          const response = await userRegister(userData);
-          setUserId(response.id);
+            const response = await userRegister(userData);
+            setUserId(response.id);
         } catch (error) {
-          console.error("Error al obtener carritos:", error);
+            console.error("Error al obtener carritos:", error);
         }
-      };
+    };
 
     return (
         <AuthContext.Provider value={{ user, setUser, login, logout, register, userId }}>
