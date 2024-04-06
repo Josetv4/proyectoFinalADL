d85f586777d8aa622b07b1897a4058a4601b6f52
Yamil Castillo <yamilcs@gmail.com>
2024-04-05 23:45:02 -0300

    agregando mdw Ilogin a las Route

--- server/config/routes/productRoutes.js
+++ server/config/routes/productRoutes.js
@@ -14,6 +14,8 @@ import { getActivity } from "../../middlewares/reports.js";
 
 import { upload } from "../../middlewares/uplodafile.js"
 
+import { isLogin } from "../../middlewares/isLogin.js"
+
 
 const router = express.Router();
 
@@ -21,10 +23,10 @@ const router = express.Router();
 router.get("/products", getActivity, getProducts )
 router.get("/products/:id", getActivity, getProductsId )
 router.get("/products/category/:id", getActivity, getProductsCategoryId )
-router.get("/products/user/:id", getActivity, getProductsByUsers )
-router.post("/products", validateParametersProducts, getActivity, upload.single('image'), createNewProduct)
-router.put("/products/:id", validateParametersProducts, getActivity, upload.single('image'), updateProducts )
-router.delete("/products/:id", getActivity, deleteProducts )
+router.get("/products/user/:id", isLogin,  getActivity, getProductsByUsers )
+router.post("/products", isLogin,  validateParametersProducts, getActivity, upload.single('image'), createNewProduct)
+router.put("/products/:id", isLogin, validateParametersProducts, getActivity, upload.single('image'), updateProducts )
+router.delete("/products/:id", isLogin, getActivity, deleteProducts )
 
 
 export default router;
