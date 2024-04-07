6305392a821a655d233c1b786cc6ac35271f90a3
Neyeska Medina <neyeska.go@gmail.com>
2024-04-07 00:43:18 -0400

    Agregando funcionalidades y estilos a la vista carrito

--- client/src/pages/ShoppingCart/ShoppingCart.jsx
+++ client/src/pages/ShoppingCart/ShoppingCart.jsx
@@ -2,23 +2,50 @@ import { Container, Grid } from "@mui/material";
 import PurchaseDetail from '../../components/PurchaseDetail/PurchaseDetail.jsx';
 import PurchaseStepper from "../../components/PurchaseDetail/PurchaseStepper.jsx";
 import PurchaseTotal from '../../components/PurchaseDetail/PurchaseTotal.jsx'
+import { useState } from "react";
 
 const ShoppingCart = () =>{   
-
+    const [ cartItems, setCartItems ] = useState([
+        {
+            id: 1,
+            fecha: "2024-03-25",
+            imagen: "https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg",
+            descripcion: "Descripción del producto 1",
+            precio: "10990",
+            quantity: 1
+        },  
+        {   
+            id: 2,
+            fecha: "2024-03-24",
+            imagen: "https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg",
+            descripcion: "Descripción del producto 2",
+            precio: "15490",
+            quantity: 3
+        },  
+        {   
+            id: 3,
+            fecha: "2024-03-23",
+            imagen: "https://www.ecofarmacias.cl/wp-content/uploads/2020/03/losartan-1-1.jpg",
+            descripcion: "Descripción del producto 3",
+            precio: "8250",
+            quantity: 6
+        },
+    ]);
+    
     return (
         <main>
-          <PurchaseStepper page={1}/>
-          <Container sx={{ display: "flex", flexDirection: 'row', maxHeight: '54vh', maxWidth: 'moz-available' }}>
-              <Grid container spacing={2}>
-                <Grid item xs={8}>
-                    <PurchaseDetail />
+            <PurchaseStepper page={1}/>
+            <Container sx={{ display: "flex", flexDirection: 'row', maxHeight: '58vh', maxWidth: '-moz-available', justifyContent: "center", alignItems: "center" }}>
+                <Grid container spacing={2} style={{ width: "100%" }}>
+                    <Grid item xs={8} style={{ height: "100%" }}>
+                        <PurchaseDetail cartItems={cartItems} setCartItems={setCartItems} />
+                    </Grid>
+                    <Grid item xs={4} style={{padding: "0", paddingLeft:"8px"}}>
+                        <PurchaseTotal cartItems={cartItems} />
+                    </Grid>
                 </Grid>
-                <Grid item xs={4}>
-                    <PurchaseTotal />
-                </Grid>
-              </Grid>
-          </Container>
-      </main>
+            </Container>
+        </main>
     )
 }
 
