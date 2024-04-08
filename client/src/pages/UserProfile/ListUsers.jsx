e4833554a175581ba58994c94fd8ddee0c86dabb
Yamil Castillo <yamilcs@gmail.com>
2024-04-08 18:10:08 -0400

    cambio LisUser

--- client/src/pages/UserProfile/ListUsers.jsx
+++ client/src/pages/UserProfile/ListUsers.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
-import { logout } from "../../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import swal from "sweetalert";
 import Container from "@mui/material/Container";
 import Table from "@mui/material/Table";
