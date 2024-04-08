91bce19d4a212faf4f0a6341f45f384dbe16e81c
ingrid-mo <morales.ingrid.i20@gmail.com>
2024-04-07 22:42:02 -0400

    agregue drawer a lastshoping

--- client/src/pages/LastShopping/LastShopping.jsx
+++ client/src/pages/LastShopping/LastShopping.jsx
@@ -1,13 +1,38 @@
 import ShoppingCard from "../../components/ShoppingCard/ShoppingCard.jsx"
+import Drawer from "@mui/material/Drawer";
+import ReviewComponent from "../../components/review/ReviewComponent.jsx";
+import { useState } from "react";
+import Button from "@mui/material/Button";
 
 const LastShopping = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
 
   return (
     <>
-     <main>
-        <ShoppingCard/>
+      <main>
+        <ShoppingCard>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={toggleDrawer}>
+            Comenta tus productos
+          </Button>
+        </ShoppingCard>
+        <Drawer
+          anchor="right"
+          open={isDrawerOpen}
+          onClose={toggleDrawer}
+          sx={{ width: 250 }}
+        >
+          {/* Aquí incluimos el ReviewComponent dentro del Drawer */}
+          <ReviewComponent />
+        </Drawer>
+     
       </main>
-    
     </>
   );
 };
