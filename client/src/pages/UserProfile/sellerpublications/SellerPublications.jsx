9252ae9e52bcc5bb0e196d0c714a40309989c341
Yamil Castillo <yamilcs@gmail.com>
2024-04-14 19:43:58 -0400

    modificacion al sellerPublication

--- client/src/pages/UserProfile/sellerpublications/SellerPublications.jsx
+++ client/src/pages/UserProfile/sellerpublications/SellerPublications.jsx
@@ -7,18 +7,18 @@ import { Box } from "@mui/material";
 import "./style.css";
 
 import { DataContext } from "../../../context/DataContext";
-import { formatDate } from '../../../utils/DateFormat';
+import { formatDate } from "../../../utils/DateFormat";
 
 const SellerPublications = () => {
   const { productSeller } = useContext(DataContext);
 
-  const products = [productSeller];
-
+  //const products = [productSeller];
+  console.log(productSeller);
   return (
     <div className="seller-publications">
       <h1>Tus ultimas publicaciones</h1>
       <Container>
-        {products.map((product, index) => (
+        {productSeller.product.map((product, index) => (
           <Card
             key={`${product.product_id}-${index}`}
             className="publication-card"
@@ -27,34 +27,35 @@ const SellerPublications = () => {
               <Box>
                 <img
                   className="publication-card-image"
-                  src={`http://localhost:4000/uploads/${product.products.image_url}`}
-                  alt={product.products.description}
+                  src={`http://localhost:4000/uploads/${product.image_url}`}
+                  alt={product.description}
                 />
               </Box>
             </CardContent>
             <CardContent className="publication-card-content">
               <Box className="publication-card-content">
                 <Typography variant="p" className="publication-name">
-                  {product.products.name}
+                  {product.name}
                 </Typography>
                 <Typography variant="p" className="color-description">
-                  {product.products.description}
+                  {product.description}
                 </Typography>
               </Box>
             </CardContent>
             <CardContent className="publication-box-content">
               <Box className="publication-card-content">
                 <Typography variant="p" className="publication-card-color">
-                  <span>Precio :</span> $ {product.products.price}
+                  <span>Precio :</span> $ {product.price}
                 </Typography>
                 <Typography variant="p">
-                  <span>Stock :</span> {product.products.stock}
+                  <span>Stock :</span> {product.stock}
                 </Typography>
                 <Typography variant="p">
-                  <span>Categoria :</span> {product.products.name_category}
+                  <span>Categoria :</span> {product.name_category}
                 </Typography>
                 <Typography variant="p">
-                  <span>Fecha Creacion :</span> {formatDate(product.products.create_at)}
+                  <span>Fecha Creacion :</span>{" "}
+                  {formatDate(product.create_at)}
                 </Typography>
               </Box>
             </CardContent>
